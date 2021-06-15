56f8f869eedeaba5bba65490abb43f36038209c9
Rubi Polanco <a20183238@pucp.edu.pe>
2021-06-15 02:41:12 -0500

    -

--- detallePais/detallePais.js
+++ detallePais/detallePais.js
@@ -1,19 +1,33 @@
 
 const urlParams = new URLSearchParams(window.location.search);
-const nameCountry = urlParams.get('name');
-//TODO
+//const nameCountry = urlParams.get('name');
+//const countryCode = urlParams.get('countryCode');
+//const countryCode = urlParams.get('slug');
+const nameCountry = "Peru";
+const countryCode = "PE";
+const slug = "peru";
 
 $(document).ready(function () {
     // const caseCovid = 'confimed';
+
     $("#titulo").html('Resumen del país ' + nameCountry);
-    //TODO
+    const urlbanderapais = "https://www.countryflags.io/" + countryCode + "/flat/64.png";
+    const imagen = "<img src=" + urlbanderapais + ">";  //Bandera de pais recibido
+    $("#bandera-div").append(imagen);
 
     $.ajax({
         method: "GET",
         datatype: "json",
         url: "https://restcountries.eu/rest/v2/alpha/" + countryCode
     }).done(function (data) {
-        //TODO
+        $("#capital").html(data.capital);
+        $("#population").html(data.population);
+        $("#subregion").html(data.subregion);
+        console.log("datos obtenidos")
+        console.log(data.capital);
+        console.log(data.population);
+        console.log(data.subregion);
+
     }).fail(function (err) {
         console.log(err);
         alert("ocurrió un error al cargar la página");
@@ -24,6 +38,9 @@ $(document).ready(function () {
 
 function seleccionarCasos() {
     //TODO
+    const caseCovid = $("#caseCovid").val();
+    console.log("Parámetro obtenido");
+    console.log(caseCovid);
 }
 
 function obtenerDataPais() {
@@ -32,7 +49,10 @@ function obtenerDataPais() {
         datatype: "json",
         url: "https://api.covid19api.com/total/dayone/country/" + slug + "/status/" + caseCovid
     }).done(function (data) {
-        //TODO
+        const fecha = data.fecha;
+        const fecha2 = formatDate(fecha);
+        console.log(fecha2);
+
     }).fail(function (err) {
         console.log(err);
         alert("ocurrió un error al cargar la página");
@@ -40,5 +60,5 @@ function obtenerDataPais() {
 }
 
 function formatDate(date) {
-    //TODO
+    date.toDateString();
 }
